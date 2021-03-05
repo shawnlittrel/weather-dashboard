0b89fca87c70f11d5429355dfb6f6ac36a539f6b
Shawn Littrel <shawnlittrel@protonmail.com>
2021-03-04 21:20:35 -0700

    basic HTML and CSS layout

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// //
+// OPEN WEATHER API CALL
+// https://openweathermap.org/api/one-call-api
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+//pull search term from text box
+
+//convert location to lat/long
+//need geocoder from Google Maps
+
+//input data into api call
+
+//fetch data
+
+//display on page
+
+//save location to history
